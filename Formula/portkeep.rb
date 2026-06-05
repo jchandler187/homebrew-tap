2fc8d8f4effe804994cc5b705ef06681b9f62e81
jchandler187 <jchandler187@users.noreply.github.com>
2026-06-05 17:18:02 -0400

    portkeep v0.1.1

--- Formula/portkeep.rb
+++ Formula/portkeep.rb
@@ -1,25 +1,25 @@
 class Portkeep < Formula
   desc "Port management + security for self-hosted infrastructure"
   homepage "https://github.com/jchandler187/portkeep"
-  version "0.1.0"
+  version "0.1.1"
 
   on_macos do
     if Hardware::CPU.arm?
-      sha256 "5ea1947a92c1e4a55b52bbdf15ef93fc9830e1344a3fe2059785b868e5011b59"
-      url "https://github.com/jchandler187/portkeep/releases/download/v0.1.0/portkeep_0.1.0_darwin_arm64.tar.gz"
+      sha256 "ce377ba6c9f8029868cde7868debff51292be307fe0b371d2480aba606bd746a"
+      url "https://github.com/jchandler187/portkeep/releases/download/v0.1.1/portkeep_0.1.1_darwin_arm64.tar.gz"
     else
-      sha256 "99c1754de5cc6c401730339154115a5e01d44f9a3a4ddaa89f40a8bc4dd00d06"
-      url "https://github.com/jchandler187/portkeep/releases/download/v0.1.0/portkeep_0.1.0_darwin_amd64.tar.gz"
+      sha256 "5b44f126e20cc1c1c1428a8c7896376898ef54751c29988d29e05e5d7013ac17"
+      url "https://github.com/jchandler187/portkeep/releases/download/v0.1.1/portkeep_0.1.1_darwin_amd64.tar.gz"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      sha256 "5167096c5c454b3051198f9b0aabc7b07a26f1ce3f5626f490708e47a7ba59ae"
-      url "https://github.com/jchandler187/portkeep/releases/download/v0.1.0/portkeep_0.1.0_linux_arm64.tar.gz"
+      sha256 "5740db2a540381020a6b06c665ffb84f7bde590d3ec6952df757aff953a9d84e"
+      url "https://github.com/jchandler187/portkeep/releases/download/v0.1.1/portkeep_0.1.1_linux_arm64.tar.gz"
     else
-      sha256 "76d18bbc7419dedfed0b8e384f8eff2e13f9aa5ee43134979ebe2d557d127b80"
-      url "https://github.com/jchandler187/portkeep/releases/download/v0.1.0/portkeep_0.1.0_linux_amd64.tar.gz"
+      sha256 "184a756b5d82960b919eb7992fcec270959a1dcfd8f8f1d0c5ae6d7f6fb3a9d4"
+      url "https://github.com/jchandler187/portkeep/releases/download/v0.1.1/portkeep_0.1.1_linux_amd64.tar.gz"
     end
   end
 
